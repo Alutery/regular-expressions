847d2fdbc0f8702e4fdcad3c0773a9809fb667bb
Aliya <ktotamy@gmail.com>
2019-04-08 16:46:28 +0300

    content theory v.1

--- theory/urls.py
+++ theory/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.theory, name='theory'),
+    path('', views.theory_intro, name='theory_intro'),
     path('lesson_1/', views.less1, name='less1'),
     path('lesson_2/', views.less2, name='less2'),
 ]
--- theory/views.py
+++ theory/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def theory(request):
-    return render(request, 'theory/main.html')
+def theory_intro(request):
+    return render(request, 'theory/lessons/lesson_1.html')
 
 def less1(request):
     return render(request, 'theory/lessons/lesson_1.html')
