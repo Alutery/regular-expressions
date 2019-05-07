c6cdcf49529209f5c388be6296177857b679774e
Aliya <ktotamy@gmail.com>
2019-05-07 13:40:22 +0300

    is_authenticated and url

--- loginsys/views.py
+++ loginsys/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.template.context_processors import csrf
-
+from mainApp.models import Profile
 
 def login(request):
     args = {}
@@ -25,3 +25,18 @@ def update_profile(request, user_id):
     user = User.objects.get(pk=user_id)
     user.profile.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...'
     user.save()
+
+def register(request):
+    args={}
+    args.update(csrf(request))
+    args['form'] = UserCreationForm()
+    if request.POST:
+        newuser_form = UserCreationForm(request.POST)
+        if newuser_form.is_valid():
+            newuser_form.save()
+            newuser = auth.authenticate(username=newuser_form.cleaned_data['username'], password=newuser_form.cleaned_data['password'])
+            auth.login(request, newuser)
+            return redirect('/')
+        else:
+            args['form'] - newuser_form
+    return render(request, 'mainApp/home.html', args)
