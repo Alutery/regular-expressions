e05b4049038a8dd9bbd2d505d3354fe30fa2c9ce
Aliya <ktotamy@gmail.com>
2019-05-12 11:36:27 +0300

    ChainAcceptance - clear

--- mainApp/static/js/practice/chain_acceptance.js
+++ mainApp/static/js/practice/chain_acceptance.js
@@ -1,6 +1,4 @@
-var categoryID = 1;
-var n = 5;
-
+var n;
 var graphs;
 var statements;
 var numbers;
@@ -11,14 +9,16 @@ function getData() {
     async: false,
     url: '../gettask/',
     type: 'post',
-    data: ({ taskType : 'ChainAcceptance'}),
-    success: function( data ){
+    data: ({
+      taskType: 'ChainAcceptance'
+    }),
+    success: function (data) {
       graphs = data.description;
       statements = data.description2;
       numbers = data.numbers;
     },
-    error: function( jqXhr, textStatus, errorThrown ){
-        console.log( errorThrown );
+    error: function (jqXhr, textStatus, errorThrown) {
+      console.log(errorThrown);
     }
   });
 }
@@ -41,9 +41,6 @@ $(document).ready(function () {
     svg_div.html("<br>loaing...<br>");
     statement.text(statement_input);
     number = numberID;
-    
-    // current_answer = answer;
-    // Generate the Visualization of the Graph into "svg".
 
     var viz = new Viz();
     viz.renderSVGElement(data)
@@ -51,16 +48,12 @@ $(document).ready(function () {
         svg_div.html(element);
       })
       .catch(error => {
-        // Create a new Viz instance (@see Caveats page for more info)
         viz = new Viz();
-
-        // Possibly display the error
         console.error(error);
       });
   }
-  // Startup function: call UpdateGraphviz
+
   jQuery(function () {
-    // event.preventDefault();
     getData();
 
     for (let i = 1; i <= n; i++) {
@@ -68,14 +61,13 @@ $(document).ready(function () {
         $(".page-item").removeClass("active");
         $(this).addClass("active");
         current = i;
-        UpdateGraphviz(graphs[i-1], statements[i-1], numbers[i-1]);
+        UpdateGraphviz(graphs[i - 1], statements[i - 1], numbers[i - 1]);
       });
     }
 
     $('#btn_task_1').click();
   });
 
-  // Bind actions to form buttons.
   btn_yes.click(function () {
     $('#answer_request').hide();
     displayResult(true);
@@ -91,68 +83,24 @@ $(document).ready(function () {
       async: false,
       url: '',
       type: 'post',
-      data: ({ taskType : 'ChainAcceptance', answer: answer, number: number}),
-      success: function( data ){
-        if(data.correct) {
+      data: ({
+        taskType: 'ChainAcceptance',
+        answer: answer,
+        number: number
+      }),
+      success: function (data) {
+        if (data.correct) {
           send_result(current - 1, n, categoryID);
           $('#result').text('Верно!');
           $('#result').css("color", "green");
-        }
-        else {
+        } else {
           $('#result').text('Не верно!');
           $('#result').css("color", "red");
         }
       },
-      error: function( jqXhr, textStatus, errorThrown ){
-          console.log( errorThrown );
+      error: function (jqXhr, textStatus, errorThrown) {
+        console.log(errorThrown);
       }
     });
   }
-
-  
-
-  // (a+b)*
-  // btn_1.click(function () {
-  //   $(".page-item").removeClass("active");
-  //   $(this).addClass("active");
-  //   current = 1;
-  //   UpdateGraphviz(data[0], statements[0], answers[0]);
-  // });
-
-  // b*(cb+a)
-  // btn_2.click(function () {
-  //   $(".page-item").removeClass("active");
-  //   $(this).addClass("active");
-  //   current = 2;
-  //   UpdateGraphviz(
-  //     data[1], statements[1], answers[1]);
-  // });
-
-  // (ab|b)(a|c*)
-  // btn_3.click(function () {
-  //   $(".page-item").removeClass("active");
-  //   $(this).addClass("active");
-  //   current = 3;
-  //   UpdateGraphviz(
-  //     data[2], statements[2], answers[2])
-  // });
-
-  // (bb(d+a)a)*
-  // btn_4.click(function () {
-  //   $(".page-item").removeClass("active");
-  //   $(this).addClass("active");
-  //   current = 4;
-  //   UpdateGraphviz(
-  //     data[3], statements[3], answers[3]);
-  // });
-
-  // (d|a(b|d))*|c|a
-  // btn_5.click(function () {
-  //   $(".page-item").removeClass("active");
-  //   $(this).addClass("active");
-  //   current = 5;
-  //   UpdateGraphviz(
-  //     data[4], statements[4], answers[4]);
-  // });
-
 });
