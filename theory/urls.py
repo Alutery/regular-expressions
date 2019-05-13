4090f9b81eaa322d57f46d6626438fae9f7bb646
Aliya <ktotamy@gmail.com>
2019-05-13 10:36:17 +0300

    basicDefinitions edited

--- theory/urls.py
+++ theory/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('basicDefinitions/', views.basic_definitions, name='basic_definitions'),
     path('regexEquivalence/', views.regexEquivalence, name='regex_equivalence'),
     path('FA/', views.FA, name='FA'),
-    path('automatonLanguages/', views.automatonLanguages, name='automaton_languages'),
     path('NFAToDFA/', views.NFAToDFA, name='NFA_to_DFA'),
     path('regextoNFA/', views.regexToNFA, name='regex_to_NFA'),
     path('DFAtoregex/', views.DFAtoregex, name='DFA_to_regex'),
