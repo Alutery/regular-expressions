a6f48bb3cdb87c9043d1c54934314c42cde8f91d
Aliya <ktotamy@gmail.com>
2019-05-15 01:07:45 +0300

    new app calculator

---
+++ calculator/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import CheckEquivalence
+from practice.lib import AutomataTheory
+from queue import Queue
+from django.http import JsonResponse
+from django.views import View  # import the View parent class
+
+from mainApp.models import CompletedQuestions, QuestionCategory, Tasks
+
+
+class Calculator(View):
+    
+    def get(self, request):
+        return render(request, 'calculator/main.html')
+
+    def post(self, request):
+        return JsonResponse({'correct': check(request)})
